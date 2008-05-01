f70476e2c72d1bcfd047a16edf97246408c51659
Stefan Bienert <bienert@zbh.uni-hamburg.de>
2008-05-02 00:06:42 +0200

    progress on reformat.pl

--- share/misc/bin/reformat.pl
+++ share/misc/bin/reformat.pl
@@ -1,7 +1,7 @@
 #!@PERL@ -w
 # -*- perl -*-
 # @configure_input@
-# Last modified: 2008-06-09.10
+# Last modified: 2008-06-09.11
 
 
 # Copyright (c) 2007 Stefan Bienert <bienert@zbh.uni-hamburg.de>
@@ -36,7 +36,6 @@ use Pod::Usage;
 
 # CONSTANTS        - BEGIN
 my $M4_COMMENT         = "\\#";
-
 # CONSTANTS        - END
 
 # GLOBALS          - BEGIN
@@ -58,7 +57,6 @@ my %Define_End         = (
 my $Verbose          = 0;
 my $Bckext           = "bkp";
 my $Dwidth           = 1;
-
 my $Exit_On_Error    = 0;
 # GLOBALS          - END
 
@@ -78,7 +76,7 @@ sub define_preamble_m4(\$ \$ \@)
 
     for (my $i = 0; $i <= $#{$lines_ref}; $i++)
     {
-        if (@{$lines_ref}[$i] =~ /(^$M4_COMMENT\s*$|
+        if (@{$lines_ref}[$i] =~ /(^\s*$M4_COMMENT\s*$|
                                    Last\s+modified\:|
                                    Copyright\s+\(C\)\s+\d+|
                                    See\s+COPYING\s+file\s+in\s+the\s+top\s+
@@ -172,6 +170,7 @@ sub format_preamble_m4($ $ $ $ \@)
         $end = $#{$lines_aryref} + 1;
     }
 
+<<<<<<< HEAD:share/misc/bin/reformat.pl
     $i = $start;
     $line = @{$lines_aryref}[$i - 1];
     if ($line !~ /^${M4_COMMENT}$/)
@@ -311,21 +310,143 @@ sub format_preamble_m4($ $ $ $ \@)
         }
         #warning_msg(sprintf ("line %*d: $line", $Dwidth, $i));
         warning_message (sprintf ("line %*d: $line", $DWIDTH, $i));
-    $i = $start;
+  $i = $start;
     $line = @{$lines_aryref}[$i - 1];
-    if ($line !~ /^$M4_COMMENT$/)
+    if ($line !~ /^${M4_COMMENT}$/)
     {
+        form_violation_msg("Preamble should start with an empty comment line",
+                           $file, $i);
         check_empty_line($line, $file, $i);
+        check_txt_before_comment($line, $M4_COMMENT, $file, $i);
+        check_txt_after_comment($line, $M4_COMMENT, $file, $i);
         check_leading_spaces($line, $M4_COMMENT, $file, $i);
         check_trailing_spaces($line, $file, $i);
-        # text before comment
-        form_violation_msg("BLA", $file, $i, 0);
+        exit(1) if ($exit);
     }
+    $i = $i + 1;
 
-    for ($i = $start; $i <= $end; $i++)
+    # check copyright line
+    for (; $i <= $end; $i++)
     {
+        $width = 0;
         $line = @{$lines_aryref}[$i - 1];
-        warning_msg(sprintf ("line %*d: $line", $Dwidth, $i));
+
+        if (   ($state eq "copyright") 
+            && (   ($line !~ /^${M4_COMMENT} Copyright \(C\) \d{4} [^\s+]/)
+                && ($line !~ /^\s*${M4_COMMENT}\s*$/)))
+        {
+            if ($i - $start == 1)
+            {
+                form_violation_msg("Preamble should be followed by copyright ".
+                                   "notes", $file, $i);
+            }
+            else
+            {
+                 form_violation_msg("Copyright notes should be of form \"".
+                                    "\# Copyright \(C\) YYYY name\"",
+                                    $file, $i);
+            }
+
+            check_empty_line($line, $file, $i);
+            check_txt_before_comment($line, $M4_COMMENT, $file, $i);
+            check_leading_spaces($line, $M4_COMMENT, $file, $i);
+            check_trailing_spaces($line, $file, $i);
+
+            # check copyright line
+            if ($line =~ /^${M4_COMMENT}(\s*)(Copyright)(\s*)(\(C\))(\s*)
+                          ([\d\.]+)(\s*)([^\s+])/xi)
+            {
+                $width = 1;
+                # check no. of ws between "#" and "Copyright"
+                check_whitespaces($1, 1, $file, $i, $width);
+                $width += length($1);
+
+                if ($2 ne "Copyright") # check spelling of "Copyright"
+                {
+                    form_violation_msg("\"Copyright\" misspelled \(\"$2\"\)",
+                                       $file, $i, $width);
+                }
+                $width += length($2);
+
+                # check no. of ws between "Copyright" and "(C)"
+                check_whitespaces($3, 1, $file, $i, $width);
+                $width += length($3);
+
+                if ($4 ne "\(C\)") # check spelling of "(C)"
+                {
+                    form_violation_msg("\"\(C\)\" symbol misspelled \(\"$4\"\)",
+                                       $file, $i, $width);
+                }
+                $width += length($4);               
+
+                # check no. of ws between "(C)" and year
+                check_whitespaces($5, 1, $file, $i, $width);
+                $width += length($5);
+
+                if ($6 ne "\d\d\d\d") # check year
+                {
+                    form_violation_msg("Wrong format of year \(\"$6\"\), ".
+                                       "should be \"YYYY\"", $file, $i, $width);
+                }
+                $width += length($6);                
+
+                # check no. of ws between "YYYY" and name
+                check_whitespaces($7, 1, $file, $i, $width);
+                $width += length($7);
+
+                if (length($8) == 0) # check name
+                {
+                    form_violation_msg("No name for copyright holder found",
+                                       $file, $i, $width);
+                }
+                $width += length($8); 
+            }
+
+            exit(1) if ($exit);
+        }
+        elsif (   ($state eq "licensenote") 
+            && (   ($line !~ /^\s*${M4_COMMENT}\sSee\sCOPYING\sfile\sin\sthe\s
+                              top\slevel\sdirectory\sof\sthis\stree\sfor\s
+                              licence\.$/x)
+                && ($line !~ /^\s*${M4_COMMENT}\s*$/)))
+        {
+            if (($i - $start) == 4)
+            {
+                form_violation_msg("Copyright notes should be followed by a ".
+                                   "reference to the licence of form \"See ".
+                                   "COPYING file in the top level directory ".
+                                   "of this tree for licence.\"", $file, $i);
+            }
+
+#    a
+# Copyright (C) 2008 Stefan Bienert
+# Copyright (C) 2008 Center for Bioinformatics, University of Hamburg
+#
+# See COPYING file in the top level directory of this tree for licence.
+#
+# Last modified: 2008-04-26.23
+#
+            print $i-$start.":".$line;
+        }
+        elsif ($line =~ /^\s*${M4_COMMENT}\s*$/)
+        {
+            if ($state eq "copyright")
+            {
+                $state = "licensenote"
+            }
+
+            if ($line !~ /^${M4_COMMENT}$/)
+            {
+                check_leading_spaces($line, $M4_COMMENT, $file, $i);
+                check_trailing_spaces($line, $file, $i);
+                exit(1) if ($exit);
+            }
+        }
+        else
+        {
+            #print $line;
+        }
+        #warning_msg(sprintf ("line %*d: $line", $Dwidth, $i));
     }
 
     return 0;
@@ -499,9 +620,7 @@ sub form_violation_msg($ $ $)
         $msg .= " ";        
     }
 
-
     $msg .= "${message}\n";
-
 
     die($msg) if ($Exit_On_Error);
 
@@ -574,7 +693,6 @@ sub parseargs(\% \$)
     $optcatchresult = GetOptions(
                               'format=s' => \$argument_hashref->{format},
                               'werror!'  => \$argument_hashref->{exit_on_error},
-
                               'verbose!' => \$Verbose,
                               'help'     => \$help,
                               'man'      => \$man
@@ -731,6 +849,8 @@ foreach $current_file (@{$arg_hash{files}})
         $extended = &{$FORMAT_PREAMBLE{$format}} (
                                               $file_structure{'preamble_start'},
                                               $file_structure{'preamble_end'},
+                                              ${current_file},
+                                              $arg_hash{exit_on_error},
                                               \@lines
                                                 );
     }

7c6475875ced35ee573061473e82d01374845b4c
Stefan Bienert <bienert@zbh.uni-hamburg.de>
2008-11-01 13:16:57 +0100

    src/libcrbbasic/created gfile.[c,h] and added to Makefile.am

---
+++ src/libcrbbasic/gfile.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2008 Stefan Bienert
+ *
+ * This file is part of CoRB.
+ *
+ * CoRB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CoRB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CoRB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ ****   Documentation header   ***
+ *
+ *  @file libcrbbasic/gfile.h
+ *
+ *  @brief Generic file handling
+ *
+ *  Module: gfile
+ *
+ *  Library: crbbasic
+ *
+ *  Project: CoRB - Collection of RNAanalysis Binaries
+ *
+ *  @author Stefan Bienert
+ *
+ *  @date 2008-11-01
+ *
+ *
+ *  Revision History:
+ *         - 2008Nov01 bienert: created
+ *
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef GFILE_H
+#define GFILE_H
+
+
+enum GFile_Types 
+{
+   GFILE_ASCII
+};
+
+typedef struct GFile GFile;
+
+
+#endif /* GFILE_H */
+
+#ifdef __cplusplus
+}
+#endif
